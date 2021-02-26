2aa8e5563f74443c59639f8bc8a6fe596f65c43b
brunaoliveira <brunaol678@gmail.com>
2021-02-26 09:18:42 -0300

    use the model class to create a new instance of a user

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys')
+require('./services/passport');
+require('./models/User');
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
-require('./services/passport');
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
