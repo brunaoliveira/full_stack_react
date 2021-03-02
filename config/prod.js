0a4b1c8cc402cbf145398e76d78075aafd6e34ed
brunaoliveira <brunaol678@gmail.com>
2021-03-02 15:24:16 -0300

    put proxy back

--- config/prod.js
+++ config/prod.js
@@ -2,6 +2,5 @@ module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
-    cookieKey: process.env.COOKIE_KEY,
-    callbackURL: 'https://protected-hollows-99036.herokuapp.com/auth/google/callback'
+    cookieKey: process.env.COOKIE_KEY
 }
